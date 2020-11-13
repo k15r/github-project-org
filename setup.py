d0bdcc383c3ab96dc88cf54db2b667072e58d390
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-11-13 08:19:10 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='github-project-org',
+    version='',
+    packages=[],
+    install_requires=['PyGithub'],
+    url='',
+    license='',
+    author='k15r',
+    author_email='',
+    description=''
+)
